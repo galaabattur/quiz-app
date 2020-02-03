6da8fc9a35810f6e7fd89d8bc1dee6e025225d68
galbadrakh_b <b.galbadrah12@gmail.com>
2020-02-02 23:36:29 -0600

    bodyparser added, now can read query strings from post method form data.

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const express = new require("express");
 const app = express();
 const Quiz = require("./models/Quiz");
+const bodyParser = require("body-parser");
 
 app.set("view engine", "pug");
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  console.log(req.body.answer);
-
   const quiz = new Quiz();
   const score = quiz.getScore();
   const question = quiz.getQuestion();
   res.render("index", { score: score, question: question });
 });
 
-app.post("/answer", (req, res) => {
+app.post("/", (req, res) => {
+  console.log("answer path");
   console.log(req.body.answer);
 });
 
